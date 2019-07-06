71e5938b4a4eef2afa8dc4a35c6bcbebbd7b6057
alle-zhinzher <sanches200000@gmail.com>
2019-07-06 14:14:45 +0300

    ReactDOM & JSX

---
+++ React/lesson1/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+    </ul>, document.getElementById('root'));
+
+
