87b5d6bd0e1bd37108dd840e74dfc10eb6d9054e
alle-zhinzher <sanches200000@gmail.com>
2019-07-06 19:20:32 +0300

    Todo app step 2

--- React/lesson1/src/components/App.js
+++ React/lesson1/src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import ToDoItem from './ToDoItem';
+
 
 
 function App() {
     return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
+        <div className="todo-list">
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
         </div>
     )
 }
