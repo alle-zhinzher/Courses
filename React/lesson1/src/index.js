8094e8d128aa17f9b359986fe7e3b38c1c32210c
alle-zhinzher <sanches200000@gmail.com>
2019-07-06 14:49:00 +0300

    Functional compnents

--- React/lesson1/src/index.js
+++ React/lesson1/src/index.js
@@ -2,13 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-    </ul>, document.getElementById('root'));
+function MyName() {
+    return <h1>Oleksandr</h1>
+};
+function MyText() {
+    return <p>Im student who lern React</p>
+}
+function MySport() {
+    var ulStyle = {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: '20px',
+    }
+    return (
+        <ul style={ulStyle}>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>
+    ) 
+ }
+
+function MyInfo() {
+    return (
+        <div>
+            <MyName />
+            <MyText />
+            <MySport />
+        </div>
+    )
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 
