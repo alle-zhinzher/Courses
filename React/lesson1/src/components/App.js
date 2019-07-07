d3cd72769b2d5620e625f8665c88bd1b260d2472
alle-zhinzher <sanches200000@gmail.com>
2019-07-07 20:38:41 +0300

    All Form in react

--- React/lesson1/src/components/App.js
+++ React/lesson1/src/components/App.js
@@ -6,21 +6,38 @@ class App extends React.Component {
         this.state = {
             firstName: "",
             lastName: "",
+            isTrue: true,
+            gender: "",
+            fawColor: "",
         }
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange (event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+        const { name, value, type, checked } = event.target
+        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({[name]: value})
+
     }
+    
     render() {
+        const st = {
+            background: "blue"
+        }
         return (
             <form >
                 <input type="text" name="firstName" placeholder="First name" onChange={this.handleChange}/>
                 <input type="text" name="lastName" placeholder="Last name" onChange={this.handleChange} />
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
+                <input type="checkbox" name="isTrue" checked={this.state.isTrue} onChange={this.handleChange} /> Make blue? <hr/>
+                <input type="radio" name="gender" value="male" checked={this.state.gender==="male"} onChange={this.handleChange} />Male <hr />
+                <input type="radio" name="gender" value="female" checked={this.state.gender==="female"} onChange={this.handleChange} />Female <hr />
+                <select value={this.state.fawColor} onChange={this.handleChange} name="fawColor">
+                    <option value="blue">1</option>
+                    <option value="red">2</option>
+                    <option value="green">3</option>
+                </select>
+                <h1>{this.state.fawColor}</h1>
+                <h1 style={this.state.isTrue ? st: {}}>{this.state.firstName} {this.state.lastName}</h1>
             </form>
+
         )
     }
 }
